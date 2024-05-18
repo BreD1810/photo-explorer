768a3e4617a3242ccf629c2d3cd5d51abe9b22e2
Bradley Garrod <bradleybredgarrod@gmail.com>
2024-05-18 18:23:19 +0100

    feat: average command

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,27 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
-#[derive(Parser, Debug)]
-pub struct Args {
+#[derive(Debug, Parser)]
+#[command(name = "photo-explorer")]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(about = "A tool for finding information about a directory of photos")]
+pub struct Cli {
+    #[command(flatten)]
+    pub common: CommonArgs,
+    #[command(subcommand)]
+    pub command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Displays the averages for metadata on the photos
+    Average(CommonArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct CommonArgs {
     /// The directory to search for photos
-    #[arg()]
+    #[arg(default_value = ".")]
     pub directory: String,
-
     /// Provides more information when running
     #[arg(short, long)]
     pub verbose: bool,
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,15 @@
 use std::fs;
 
 use clap::Parser;
-use cli::Args;
+use cli::{Cli, Command};
 
 mod cli;
 
 pub fn run() {
-    let args = Args::parse();
-
-    if args.verbose {
-        println!("Searching: {}", args.directory);
-    }
-
-    let paths = fs::read_dir(&args.directory);
-    if let Err(e) = paths {
-        eprintln!("Unable to read directory {}", args.directory);
-        eprintln!("Error: {}", e);
-        std::process::exit(1);
-    }
-
-    for path in paths.unwrap() {
-        let path_string = path.unwrap().path().display().to_string();
-        let file_infos = extract_file_info(&path_string);
-        for info in file_infos.iter() {
-            print_info(info, &path_string);
-        }
+    let args = Cli::parse();
+    match args.command {
+        Some(Command::Average(avg_args)) => run_average(&avg_args.directory, avg_args.verbose),
+        None => run_default(&args.common.directory, args.common.verbose),
     }
 }
 
@@ -84,4 +69,94 @@ fn print_info(info: &FileInfo, path: &str) {
         }
     }
     println!()
+}
+
+fn run_default(directory: &str, verbose: bool) {
+    if verbose {
+        println!("Searching: {}\n", directory);
+    }
+
+    let paths = fs::read_dir(&directory);
+    if let Err(e) = paths {
+        eprintln!("Unable to read directory {}", directory);
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+
+    for path in paths.unwrap() {
+        let path_string = path.unwrap().path().display().to_string();
+        let file_infos = extract_file_info(&path_string);
+        for info in file_infos.iter() {
+            print_info(info, &path_string);
+        }
+    }
+}
+
+fn run_average(directory: &str, verbose: bool) {
+    if verbose {
+        println!("Searching: {}\n", directory);
+    }
+
+    let paths = fs::read_dir(&directory);
+    if let Err(e) = paths {
+        eprintln!("Unable to read directory {}", directory);
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+
+    let mut apertures: Vec<f64> = Vec::new();
+    let mut focal_lens: Vec<f64> = Vec::new();
+    let mut isos: Vec<i32> = Vec::new();
+    // let mut shutter_speeds: Vec<String> = Vec::new();
+
+    for path in paths.unwrap() {
+        let path_string = path.unwrap().path().display().to_string();
+        let file_infos = extract_file_info(&path_string);
+        for info in file_infos.iter() {
+            if let Some(a) = info.aperture {
+                if verbose {
+                    println!("Aperture for {} is {}", path_string, a)
+                }
+                apertures.push(a)
+            }
+            if let Some(fl) = info.focal_length {
+                if verbose {
+                    println!("Focal length for {} is {}", path_string, fl)
+                }
+                focal_lens.push(fl)
+            }
+            if let Some(iso) = info.iso {
+                if verbose {
+                    println!("ISO for {} is {}", path_string, iso)
+                }
+                isos.push(iso)
+            }
+            // if let Some(ss) = &info.shutter_speed {
+            //     if verbose {
+            //         println!("Shutter speed for {} is {}", path_string, ss)
+            //     }
+            //     shutter_speeds.push(ss.to_string())
+            // }
+            if verbose {
+                println!()
+            }
+        }
+    }
+
+    println!(
+        "Average aperture: {}",
+        apertures.iter().sum::<f64>() / apertures.len() as f64
+    );
+    println!(
+        "Average focal length: {}",
+        focal_lens.iter().sum::<f64>() / focal_lens.len() as f64
+    );
+    println!(
+        "Average ISO: {}",
+        isos.iter().sum::<i32>() / isos.len() as i32
+    );
+    // println!(
+    //     "Average shutter speed: {}",
+    //     shutter_speeds.iter().sum() / shutter_speeds.len()
+    // );
 }
